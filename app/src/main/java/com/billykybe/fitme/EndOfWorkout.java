4ad3d3f35ab278cafc84b397fc7ca26fe5bb6c17
BILLY K <billykybe@gmail.com>
2022-06-17 09:44:33 +0300

    Fix Homescreen

---
+++ app/src/main/java/com/billykybe/fitme/EndOfWorkout.java
@@ -0,0 +1,13 @@
+package com.billykybe.fitme;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EndOfWorkout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end_of_workout);
+    }
+}

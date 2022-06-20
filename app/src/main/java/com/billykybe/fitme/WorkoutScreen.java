6e51773c31f36fb0e9f3957f2e7d049c9a8a5955
BILLY K <billykybe@gmail.com>
2022-06-20 05:20:13 +0300

    Achievement  added

--- app/src/main/java/com/billykybe/fitme/WorkoutScreen.java
+++ app/src/main/java/com/billykybe/fitme/WorkoutScreen.java
@@ -1,17 +1,14 @@
 package com.billykybe.fitme;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
-import android.os.Looper;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.airbnb.lottie.LottieAnimationView;
 
@@ -22,77 +19,83 @@ import java.util.TimerTask;
 
 public class WorkoutScreen extends AppCompatActivity {
 
-List<Workout_items_model> workout_list = new ArrayList<>();
-String id;
-    ProgressBar progressBar ;
-    TextView counter ;
+    public  List<Workout_items_model> workout_list = new ArrayList<>();
+    String id;
+    ProgressBar progressBar;
+    TextView counter;
     LottieAnimationView wImage;
     TextView wText;
-    int modifier=0;
+    int modifier = 0;
     int size;
+    int active;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_screen);
-wImage= findViewById(R.id.wi_img);
-wText =  findViewById(R.id.ws_title);
+        wImage = findViewById(R.id.wi_img);
+        wText = findViewById(R.id.ws_title);
         Bundle intent = getIntent().getExtras();
 
-        id =intent.getString("id");
+        id = intent.getString("id");
         //update list of items
-addItems(id);
-size = 0;
+        addItems(id);
+
         progressBar = findViewById(R.id.ws_progressbar);
         counter = findViewById(R.id.wscounter_text);
-wText.setText("hh");
 
-for (int i = 1 ;i<workout_list.size();i++){
-//            displayWorkout(workout_list.get(i));
-            trytwo(workout_list.get(i));
+for (int i = 1 ;i < workout_list.size();i++) {
+    Toast.makeText(WorkoutScreen.this, "Act "+workout_list.get(i).getW_name(), Toast.LENGTH_SHORT).show();
+
+    CountDownTimer countDownTimer = new CountDownTimer(Integer.parseInt(workout_list.get(i).w_duration)* 1000, 1000) {
+int current = 1;
 
 
+        @Override
+        public void onTick(long l) {
 
+            counter.setText(String.valueOf(l/1000));
+
+
+            
+            
         }
 
+        @Override
+        public void onFinish() {
+            current++;
+
+
+            wImage.setAnimation(workout_list.get(current).getW_lottie());
+            wText.setText(workout_list.get(current).getW_name());
+           if (current == workout_list.size()){
+               Intent intent1 = new Intent(getApplicationContext(), EndOfWorkout.class);
+               startActivity(intent1);
+               finish();
+           }
+        }
+    };
+    countDownTimer.start();
+
+
+}
+
+
 
 
 
 
     }
-public  int counterOn;
-    private void trytwo(Workout_items_model workout_items_model) {
-        new CountDownTimer(3000,1000){
-            public void onTick(long millisec){
-                wText.setText( workout_items_model.getW_name());
-                Toast.makeText(WorkoutScreen.this, String.valueOf(counterOn), Toast.LENGTH_SHORT).show();
-                wImage.setImageResource(workout_items_model.getW_lottie());
-counterOn++;
-            }
-
-            @Override
-            public void onFinish() {
-                 wText.setText( "Finnished");
-                 Intent toCongrats = new Intent(getApplicationContext(),EndOfWorkout.class);
-                 startActivity(toCongrats);
-                 finish();
-            }
 
 
-        }.start();
-    }
-
-
-
-
-
-    public  void addItems(String id){
-        switch (id){
+    public void addItems(String id) {
+        switch (id) {
             case "fullbody-bg":
                 workout_list.clear();
                 workout_list.add(new Workout_items_model(R.raw.spaceman, "Kicks", "10"));
-                workout_list.add(new Workout_items_model(R.raw.spaceman, "2icks", "15"));
-                workout_list.add(new Workout_items_model(R.raw.spaceman, "3icks", "20"));
-                workout_list.add(new Workout_items_model(R.raw.spaceman, "4icks", "10"));
+                workout_list.add(new Workout_items_model(R.raw.trophy, "4icks", "15"));
+
+                workout_list.add(new Workout_items_model(R.raw.likeicon, "2icks", "20"));
+                workout_list.add(new Workout_items_model(R.raw.congratulations, "3icks", "25"));
 
 
                 break;
@@ -104,10 +107,10 @@ counterOn++;
             case "abs-bg":
                 workout_list.clear();
 
-         break;
+                break;
             case "chest-bg":
 
-          break;
+                break;
 
         }
     }
